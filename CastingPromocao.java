84577be3a3cb03efbb1c78827aacee30d012b74c
Peter Thomas <peterpetrick@gmail.com>
2017-03-07 13:45:26 -0300

    CastingPromocao

---
+++ CastingPromocao.java
@@ -0,0 +1,29 @@
+/**
+ * CastingPromocao
+ */
+public class CastingPromocao {
+
+    public static void main(String[] args) {
+        double d = 3.1415;
+        int i = d;
+        //não compila
+
+        d = 5;
+        // ok, o double pode conter um número inteiro
+
+        i = d;
+        // não compila
+
+        i = 5;
+        double d2 = i;
+        // compila
+
+        // Casting
+        double d3 = 3.14;
+        i = (int) d3;
+
+        // Casting
+        long x = 10000;
+        int i = (int) x;
+    }
+}
