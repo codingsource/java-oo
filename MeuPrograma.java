47e0ea383a0fec815471a9513b64fed6dcd2930e
Peter Thomas <peterpetrick@gmail.com>
2017-03-07 13:02:40 -0300

    Adicionando no arquivo .java para teste

--- MeuPrograma.java
+++ MeuPrograma.java
@@ -1,4 +1,4 @@
-class MeuPrograma {
+public class MeuPrograma {
     public static void main (String[] args) {
         System.out.println("Minha primaeira aplicação Java!!");
         System.out.println("Minha primaeira aplicação Java!!");
