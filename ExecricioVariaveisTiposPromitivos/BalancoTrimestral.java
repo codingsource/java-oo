1abecade7bdea3eb954e98065ccbb5e2de5188ef
Peter Thomas <peterpetrick@gmail.com>
2017-03-07 13:44:51 -0300

    Exercicio de variaveis

---
+++ ExecricioVariaveisTiposPromitivos/BalancoTrimestral.java
@@ -0,0 +1,15 @@
+/**
+ * BalancoTrimestral
+ */
+public class BalancoTrimestral {
+    public static void main(String[] args) {
+
+        int gastosJaneiro = 15000;
+        int gastosFevereiro = 23000;
+        int gastosMarco = 17000;
+
+        int gastoTrimestre = gastosJaneiro + gastosFevereiro + gastosMarco;
+
+        System.out.println("Gasto trimestral: " + gastoTrimestre);
+    }    
+}
