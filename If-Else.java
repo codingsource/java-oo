55db97ff54225fb2af8761a397cc603fdc715271
Peter Thomas <peterpetrick@gmail.com>
2017-03-07 13:52:24 -0300

    Adicionando condicao idade < 18  e AmigoDoDono

--- If-Else.java
+++ If-Else.java
@@ -10,7 +10,16 @@ public class If_Else {
 
         int idade = 15;
         if (idade < 18) {
-            System.out.println("Não pode Entrar");
+            System.out.println("Não pode entrar");
+        }else {
+            System.out.println("Pode entrar");
+        }
+
+        boolean amigoDoDono = true;
+        if (idade < 18 & amigoDoDono == false) {
+            System.out.println("Não pode entrar");
+        } else {
+            System.out.println("Pode entrar");
         }
     }
 }
