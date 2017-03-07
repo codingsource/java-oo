39ce0ba7c5b77cbca2483c03a20f8f6e505854b5
Peter Thomas <peterpetrick@gmail.com>
2017-03-07 11:20:39 -0300

    Primeiro exercicio

---
+++ MeuPrograma.java
@@ -0,0 +1,6 @@
+class MeuPrograma {
+    public static void main (String[] args) {
+        System.out.println("Minha primaeira aplicação Java!!");
+        System.out.println("Minha primaeira aplicação Java!!");
+    }
+}
