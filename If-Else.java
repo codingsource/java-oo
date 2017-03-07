2c64c9d7c6063ece10ee52837961ac9912ff0420
Peter Thomas <peterpetrick@gmail.com>
2017-03-07 13:49:28 -0300

    Exemplos IF-ELSE

---
+++ If-Else.java
@@ -0,0 +1,16 @@
+/**
+ * If_Else
+ */
+public class If_Else {
+    public static void main(String[] args) {
+        boolean condicaoBooleana = true;
+        if (condicaoBooleana) {
+            System.out.println("Verdade");
+        }
+
+        int idade = 15;
+        if (idade < 18) {
+            System.out.println("Não pode Entrar");
+        }
+    }
+}
