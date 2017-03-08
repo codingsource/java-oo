91134f37ab581bee7b57ff3da8acb5747764136f
Peter Thomas <peterpetrick@gmail.com>
2017-03-08 08:25:56 -0300

    Metodo saque

--- OrientacaoObjetos/Conta.java
+++ OrientacaoObjetos/Conta.java
@@ -3,7 +3,12 @@
  */
 public class Conta {
     int numero;
-    String  dondo;
+    String  dono;
     double saldo;
     double limite;
+    
+    void saque(double quantidade){
+        double novoSaldo = this.saldo - quantidade;
+        this.saldo = novoSaldo;
+    }
 }
