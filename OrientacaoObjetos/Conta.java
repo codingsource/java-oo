cd06a7958bb2d43aeff3d1ce1b2ec453420e6f31
Peter Thomas <peterpetrick@gmail.com>
2017-03-08 08:50:04 -0300

    Alterando metodo saque para expressao booleana

--- OrientacaoObjetos/Conta.java
+++ OrientacaoObjetos/Conta.java
@@ -2,14 +2,19 @@
  * Conta
  */
 public class Conta {
+
     int numero;
-    String  dono;
+    String dono;
     double saldo;
     double limite;
-    
-    void saque(double quantidade){
-        double novoSaldo = this.saldo - quantidade;
-        this.saldo = novoSaldo;
+
+    boolean saque(double quantidade) {
+        if (this.saldo < quantidade) {
+            return false;
+        } else {
+            this.saldo = this.saldo - quantidade;
+            return true;
+        }
     }
 
     void deposita(double quantidade) {
