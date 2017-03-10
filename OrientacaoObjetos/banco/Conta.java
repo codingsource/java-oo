8e114917b449b16aadd4c8dbee6dc99293d82407
Peter Thomas <peterpetrick@gmail.com>
2017-03-10 15:54:26 -0300

    Encapsulando atributos

--- OrientacaoObjetos/banco/Conta.java
+++ OrientacaoObjetos/banco/Conta.java
@@ -3,14 +3,56 @@
  */
 public class Conta {
 
-    int numero;
-    String dono;
-    String cpf;
-    double saldo;
-    double limite;
-    Cliente titular;
+    private int numero;
+    private String dono;
+    private String cpf;
+    private double saldo;
+    private double limite;
+    private Cliente titular;
+
+    public int geNumero() {
+        return this.numero;
+    }
+
+    public void setNumero(int numero) {
+        this.numero = numero;
+    }
+    
+    public String getDono() {
+        return this.dono;
+    }
+
+    public void setDono(String dono) {
+        this.dono = dono;
+    }
+
+    public String getCpf() {
+        return this.cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+    
+    public Double getSaldo() {
+        return this.saldo + this.limite;
+    }
+
+    public double setLimite(double limite) {
+        this.limite = limite;
+    }
+
+    public Cliente getTitular() {
+        return this.titular;
+    }
+
+    public void setTitular(Cliente titular) {
+        this.titular = titular;
+    }
+
     boolean saque(double quantidade) {
-        if (this.saldo < quantidade) {
+        if (this.saldo < quantidade || quantidade > this.saldo + this.limite) {
+            System.out.println("Saldo insuficiente");
             return false;
         } else {
             this.saldo = this.saldo - quantidade;
@@ -19,7 +61,9 @@ public class Conta {
     }
 
     void deposita(double quantidade) {
-        this.saldo += quantidade;
+        if (quantidade % 2 == 0) {
+            this.saldo += quantidade;
+        }
     }
 
     boolean tranferePara(Conta destino, double valor) {
