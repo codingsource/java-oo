34865321690a3872a7f390a8b0c72394b4501b24
Peter Thomas <peterpetrick@gmail.com>
2017-03-12 01:58:23 -0300

    GetSet class Cliente
    Adicioando get na class cliente

--- OrientacaoObjetos/banco/Cliente.java
+++ OrientacaoObjetos/banco/Cliente.java
@@ -2,15 +2,50 @@
  * Cliente
  */
 public class Cliente {
-    String nome;
-    String sobrenome;
-    String cpf;    
+    private String nome;
+    private String sobrenome;
+    private String cpf;
+    private int idade;
 
-    public Cliente (String nome, String sobrenome, String cpf) {
+    public String getNome () {
+        return this.nome;
+    }
+
+    public void setNome () {
+        this.nome = nome;
+    }
+
+     public String getSobrenome () {
+        return this.sobrenome;
+    }
+
+    public void setSobrenome () {
+        this.sobrenome = sobrenome;
+    }
+
+     public String getCpf () {
+        return this.cpf;
+    }
+
+    public void setCpf () {
+        this.cpf = cpf;
+    }
+
+
+     public int getIdade () {
+        return this.idade;
+    }
+
+    public void setIdade () {
+        this.idade = idade;
+    }
+
+    public Cliente (String nome, String sobrenome, String cpf, int idade) {
         super();
         this.nome = nome;
         this.sobrenome = sobrenome;
         this.cpf = cpf;
+        this.idade = idade;
     }
 
      public void mudaCPF(String cpf) {
